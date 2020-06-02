d20e800bb2f3a776a0a809a9fd297b8539c430ef
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-06-02 23:26:23 +0200

    The Supermarket Queue: time complexity optimization

--- js/supermarket-queue/index.js
+++ js/supermarket-queue/index.js
@@ -4,8 +4,21 @@ export const queueTime = (customers, n) => {
     let tills = Array.from({ length: n }).fill(0);
     for (let time of customers) {
         tills[0] += time;
-        tills = tills.sort((a, b) => a - b);
+        tills = sortTills(tills);
     }
 
     return tills[tills.length - 1];
+};
+
+const sortTills = tills => {
+    for (let i = 0; i < tills.length - 1; i++) {
+        if (tills[i] <= tills[i + 1]) {
+            break;
+        }
+        const buffer = tills[i];
+        tills[i] = tills[i + 1];
+        tills[i + 1] = buffer;
+    }
+
+    return tills;
 };
