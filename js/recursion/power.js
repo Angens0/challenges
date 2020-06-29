eab677039320893a477ef88ccea2c1b758db8adc
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-06-29 23:52:52 +0200

    Recursion: Power - refactorization

--- js/recursion/power.js
+++ js/recursion/power.js
@@ -2,9 +2,5 @@
 // The function should return the power of the base to the exponent.
 // This function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents.
 
-export const power = (base, exponent) => {
-  if (exponent === 1) return base;
-  if (exponent === 0) return 1;
-
-  return base * power(base, exponent - 1);
-};
+export const power = (base, exponent) =>
+  !exponent ? 1 : base * power(base, exponent - 1);
