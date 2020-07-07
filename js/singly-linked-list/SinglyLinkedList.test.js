e2ed3e998e20e4daf8c3dfa1b9640384cdc280db
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-07-07 21:19:47 +0200

    Singly Linked List: Pop Test

--- js/singly-linked-list/SinglyLinkedList.test.js
+++ js/singly-linked-list/SinglyLinkedList.test.js
@@ -25,3 +25,17 @@ test("push", () => {
 
     expect(current).toBeNull();
 });
+
+test("pop", () => {
+    const { list, array } = createTestSuite();
+
+    while (array.length) {
+        expect(list.pop()).toEqual(array.pop());
+        expect(list.length).toEqual(array.length);
+    }
+
+    expect(list.length).toEqual(0);
+    expect(list.tail).toBeNull();
+    expect(list.head).toBeNull();
+    expect(list.pop()).toBeUndefined();
+});
