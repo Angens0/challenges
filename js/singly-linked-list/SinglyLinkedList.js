b34f5879bfe96c2acf598ab2d7f1126add6a88b6
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-07-07 19:30:42 +0200

    Singly Linked List: Push

---
+++ js/singly-linked-list/SinglyLinkedList.js
@@ -0,0 +1,24 @@
+import { Node } from "./Node";
+
+export class SinglyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(value) {
+        const node = new Node(value);
+
+        if (this.length === 0) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+        this.length++;
+
+        return this;
+    }
+}
---
+++ js/singly-linked-list/index.js
@@ -0,0 +1,13 @@
+import { SinglyLinkedList } from "./SinglyLinkedList";
+
+const list = new SinglyLinkedList();
+console.log(list);
+
+list.push(5);
+console.log(list);
+
+list.push(7);
+console.log(list);
+
+list.push(25);
+console.log(list);
