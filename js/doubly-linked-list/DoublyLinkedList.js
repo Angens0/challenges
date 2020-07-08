8940cb8b0a4cda5cd198529fea903dbf3fa8fdcc
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-07-08 21:27:51 +0200

    Doubly Linked List: Pop

--- js/doubly-linked-list/DoublyLinkedList.js
+++ js/doubly-linked-list/DoublyLinkedList.js
@@ -20,4 +20,25 @@ export class DoublyLinkedList {
         this.tail = node;
         this.length++;
     }
+
+    pop() {
+        if (this.length === 0) {
+            return undefined;
+        }
+
+        const last = this.tail;
+        const prev = last.prev;
+        this.tail = prev;
+        this.length--;
+
+        if (this.length === 0) {
+            this.head = null;
+        } else {
+            prev.next = null;
+        }
+
+        // prevents list.pop().prev if pop() returns a node or reference to the node exists outside of the list
+        last.prev = null;
+        return last.value;
+    }
 }
