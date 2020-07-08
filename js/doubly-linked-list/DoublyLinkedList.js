506af9d90f92703dd37258e911c3238eeaee8e6b
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-07-08 20:59:25 +0200

    Doubly Linked List: Push

--- js/doubly-linked-list/DoublyLinkedList.js
+++ js/doubly-linked-list/DoublyLinkedList.js
@@ -1,7 +1,23 @@
+import { Node } from "./Node";
+
 export class DoublyLinkedList {
     constructor() {
         this.length = 0;
         this.head = null;
         this.tail = null;
+    }
+
+    push(value) {
+        const node = new Node(value);
+
+        if (this.length === 0) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+        }
+
+        this.tail = node;
+        this.length++;
     }
 }
