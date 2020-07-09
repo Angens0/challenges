bc07f33a376c952e81556fc3efae03c9ff249ae2
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-07-09 22:51:22 +0200

    Binary Search Tree: Insert (ignore duplicates)

--- js/binary-search-tree/BinarySearchTree.js
+++ js/binary-search-tree/BinarySearchTree.js
@@ -4,4 +4,26 @@ export class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(value) {
+        const node = new Node(value);
+
+        if (!this.root) {
+            this.root = node;
+            return this;
+        }
+
+        let current = this.root;
+        while (true) {
+            if (value == current.value) return undefined;
+            const side = value < current.value ? "left" : "right";
+
+            if (!current[side]) {
+                current[side] = node;
+                return this;
+            }
+
+            current = current[side];
+        }
+    }
 }
